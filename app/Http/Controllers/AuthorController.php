e1e2e3e2e48250ef93437149986fce22ae370bee
DidinMahfuddin <151610194@smkassalaambandung.sch.id>
2017-08-02 12:27:02 -0700

    pembukuan

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Author;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        //
+        $author = Author::all();
+        return view('author.index', compact('author'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('author.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $author = new Author();
+        $author->nama = $request->a;
+        $author->save();
+        return redirect('author');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+
+        $author = Author::findorFail($id);
+        return view('author.show',compact('author'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $author = Author::findorFail($id);
+        return view('author.edit',compact('author'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $author = Author::findorFail($id);
+        $author->nama = $request->a;
+        $author->save();
+        return redirect('author');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $author = Author::findorFail($id);
+        $author->delete();
+        return redirect('author.index');
+    }
+}
---
+++ resources/views/author/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+<div class="row">
+	<center><h1>Data Author</h1></center>
+	<div class="panel panel-primary">
+		<div class="panel-heading">Data author
+		<div class="panel-title pull-right"><a href="{{URL::previous()}}">Kembali</a></div></div>
+
+		<div class="panel-body">
+				<div class="form-group">
+					<label class="control-lable">Nama Author</label>
+					<input type="text" name="a" class="form-control" value="{{$author->nama}}" readonly="">
+				</div>
+			</form>	
+		</div>
+	</div>
+</div>
+@endsection
